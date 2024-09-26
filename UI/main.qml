4791b9a74f301f80ca0be2b880e77af6c3adf003
Sohshun Tateishi <sohshun96@gmail.com>
2024-09-26 14:57:02 -0700

    completed building application

--- UI/main.qml
+++ UI/main.qml
@@ -3,13 +3,44 @@ import QtQuick.Controls.Basic
 
 ApplicationWindow {
     visible: true
-    width: 600
+    width: 800
     height: 500
-    title: "HelloApp"
-    
-    Text {
-        anchors.centerIn: parent
-        text: "Hello World"
-        font.pixelSize: 24
+    title: "Monaco Clock"
+
+    // Making application window frameless
+    // flags: Qt.FramelessWindowHint | Qt.Window
+
+    property string currTime: "00:00:00"
+    property QtObject backend
+
+    Rectangle {
+        anchors.fill: parent
+
+        Image {
+            sourceSize.width: parent.width
+            sourceSize.height: parent.height
+            source: ".images/monaco.jpg"
+            fillMode: Image.PreserveAspectCrop
+        }
+
+        Text {
+            anchors.centerIn: parent
+            horizontalAlignment: Text.AlignHCenter
+            text: currTime
+            font.pixelSize: 100
+            font.family: "Monaco"
+            color: "white"
+
+            width: implicitWidth
+            height: implicitHeight
+        }
+    }
+
+    Connections {
+        target: backend
+
+        function onUpdated(msg) {
+            currTime = msg
+        }
     }
 }
--- main.py
+++ main.py
@@ -1,17 +1,67 @@
 import sys
 import os 
+import threading
+from time import strftime, gmtime, sleep
 
 from PyQt6.QtGui import QGuiApplication
 from PyQt6.QtQml import QQmlApplicationEngine
 from PyQt6.QtQuick import QQuickWindow
+from PyQt6.QtCore import QObject, pyqtSignal
+
+
+class Backend(QObject):
+
+    def __init__(self):
+        QObject.__init__(self)
+
+    updated = pyqtSignal(str, arguments=['updater'])
+
+    def bootUp(self):
+        t_thread = threading.Thread(target=self._bootUp)
+        t_thread.daemon = True
+        t_thread.start()
+
+    def _bootUp(self):
+        while True:
+            curr_time = strftime("%H:%M:%S", gmtime())
+            self.updater(curr_time)
+            sleep(0.1)
+    
+    def updater(self, curr_time):
+        self.updated.emit(curr_time)
 
 QQuickWindow.setSceneGraphBackend('software')
+
 
 app = QGuiApplication(sys.argv)
 
 engine = QQmlApplicationEngine()
 engine.quit.connect(app.quit)
-engine.load('./UI/main.qml')
+
+if getattr(sys, 'frozen', False):
+    # If bundled by PyInstaller
+    base_path = sys._MEIPASS
+else:
+    # If running from source
+    base_path = os.path.dirname(os.path.abspath(__file__))
+
+qml_file = os.path.join(base_path, "UI/main.qml")
+engine.load(qml_file)
+
+if not engine.rootObjects():
+    sys.exit(-1)
+
+back_end = Backend()
+
+engine.rootObjects()[0].setProperty('backend', back_end)
+
+back_end.bootUp()
+
+
+
+# curr_time = strftime("%H:%M:%S", gmtime())
+# engine.rootObjects()[0].setProperty('currTime', curr_time)
+
 
 sys.exit(app.exec())
 
